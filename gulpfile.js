2fce1e624c0741ff7d1bb2369dc368a5dc7590a3
Chris DeLuca <bronzehedwick@gmail.com>
2015-07-06 21:08:48 -0400

    Convert tests to helper script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,21 @@
-var gulp   = require('gulp'),
-    eslint = require('gulp-eslint'),
-    bump   = require('gulp-bump'),
-    //watch  = require('gulp-watch'),
-    jspath = 'src/**/*.js';
+var gulp = require('gulp'),
+    eslint = require('gulp-eslint');
 
-// Javascript linting
-gulp.task('eslint', function(done) {
-  return gulp.src(jspath)
+// Source linting
+gulp.task('lint', function(done) {
+  return gulp.src('src/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 });
 
-// Run actions on file changes
-gulp.task('watch', function() {
-  gulp.watch([jspath], ['eslint']);
-});
-
-// Bump version number
-gulp.task('major', function() {
-  gulp.src('./package.json')
-  .pipe(bump({type:'major'}))
-  .pipe(gulp.dest('./'));
-});
-gulp.task('minor', function() {
-  gulp.src('./package.json')
-  .pipe(bump({type:'minor'}))
-  .pipe(gulp.dest('./'));
-});
-gulp.task('patch', function() {
-  gulp.src('./package.json')
-  .pipe(bump({type:'patch'}))
-  .pipe(gulp.dest('./'));
+// Tests linting
+gulp.task('lint_tests', function(done) {
+  return gulp.src('test/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
 });
 
 // Default task
-gulp.task('default', ['eslint']);
+gulp.task('default', ['lint', 'lint_tests']);
--- test/test.js
+++ test/test.js
@@ -1,62 +1,39 @@
 /* jshint browser: false, node: true */
-/* globals describe, beforeEach, afterEach, it, Robot, TextMessage */
-var path   = require('path'),
-    chai   = require('chai'),
-    expect = chai.expect;
-
-var Robot       = require('hubot/src/robot'),
-    TextMessage = require('hubot/src/message').TextMessage;
+/* globals describe, beforeEach, afterEach, it */
+var expect = require('chai').expect,
+    TextMessage = require('hubot/src/message').TextMessage,
+    helper = require('hubot-mock-adapter-helper');
 
 describe('Dungeonbot, the dungeon crawling robot', function() {
-  var robot, user, adapter;
+  var _ref = {},
+      robot = _ref.robot,
+      user = _ref.user,
+      adapter = _ref.adapter;
 
   beforeEach(function(done) {
-    // create new robot, without http, using the mock adapter
-    robot = new Robot(null, 'mock-adapter', false, 'dungeonbot');
 
-    robot.adapter.on('connected', function() {
-      // only load scripts we absolutely need, like auth.coffee
-      //process.env.HUBOT_AUTH_ADMIN = '1';
-      //robot.loadFile(
-        //path.resolve(
-          //path.join('node_modules/hubot/src/scripts')
-        //),
-        //'auth.coffee'
-      //);
+    return helper.setupRobot(function(ret) {
+      robot = ret.robot;
+      user = ret.user;
+      adapter = ret.adapter;
 
-      // load the module under test and configure it for the
-      // robot.  This is in place of external-scripts
-      require('../index')(robot);
-
-      // create a user
-      user = robot.brain.userForId('1', {
-        name: 'mocha',
-        room: '#mocha'
-      });
-
-      adapter = robot.adapter;
-
-      console.log('finishing beforeEach');
-      done();
+      return done();
     });
 
-    robot.run();
   });
 
   afterEach(function() {
-    robot.shutdown();
+    return robot.shutdown();
   });
 
-  it('enters the dungeon', function(done) {
-    // here's where the magic happens!
-    console.log('entering dungeon function');
-    adapter.on('message', function(envelope, strings) {
-      expect(strings[0]).to.include('has entered the dungeon');
-      console.log('finishing reply');
-      done();
-    });
+  return it('enters the dungeon', function(done) {
 
-    console.log('about to send text to hubot');
-    adapter.receive(new TextMessage(user, 'dungeon me'));
+    adapter.on('reply', function(envelope, strings) {
+      expect(envelope.user.name).to.equal('mocha');
+
+      return expect(strings[0]).to.include('has entered the dungeon');
+    }, done);
+
+    return adapter.receive(new TextMessage(user, 'dungeon me'));
   });
 });
